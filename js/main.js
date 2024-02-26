3bc9c19da9f7fa2820237a5134de47f59f600cee
Ganesh <ganeshrestha97@gmail.com>
2024-02-26 06:23:58 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*----- constants -----*/
+// const panels
+// const sequence
+
+
+/*----- state variables -----*/
+
+// round
+
+// sequence ?
+
+/*----- cached elements  -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+
+// function randomPanel() {
+//     const colors = ['red', 'yellow', 'green', 'blue']
+//     return colors[Math.floor]
+// }
+
+// function flashPanel
+
+// function panelClicked
+
+
+
