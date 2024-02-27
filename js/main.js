c1e424e9dc14540462fb6b6504fd091f931a52cf
Ganesh <ganeshrestha97@gmail.com>
2024-02-27 11:40:59 -0800

    seventh push

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@
 
 const sequence = []
 
-const soundRed = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3')
-const soundGreen = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3')
+const soundRed = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3')
+const soundGreen = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3')
 const soundBlue = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3')
 const soundYellow = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+
 
 /*----- state variables -----*/
 
